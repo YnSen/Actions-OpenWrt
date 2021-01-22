02a3fbc1539a1126dc77b75466311450821f2685
YnSen <65432445+YnSen@users.noreply.github.com>
2021-01-22 21:04:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#Lienol packages
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # KoolProxyR去广告插件
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 # 微信推送插件
