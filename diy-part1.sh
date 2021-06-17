23a396ee10faf30ded61af2387a9cac93addc113
YnSen <65432445+YnSen@users.noreply.github.com>
2021-06-17 11:39:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,4 +26,4 @@ sed -i '$a src-git openwrt_passwall https://github.com/xiaorouji/openwrt-passwal
 #liuruan001 pasckages
 #sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 #ssrplus
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
