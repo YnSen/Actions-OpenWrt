55d40606bdec27a328a7c76dc013518b78c799ea
YnSen <65432445+YnSen@users.noreply.github.com>
2021-06-21 06:21:24 +0800

    Delete tencentddns.lua

--- tencentddns.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.tencentddns",package.seeall)
-function index()
-entry({"admin", "network", "tencentcloud"}, firstchild(), "腾讯云设置", 30).dependent=false
-entry({"admin", "network", "tencentcloud", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"),2)
-end
