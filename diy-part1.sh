c6bc923da35ed5026c014a328bf90d1ed2081450
YnSen <65432445+YnSen@users.noreply.github.com>
2021-06-22 19:31:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,13 +16,13 @@
 
 # Add a feed source
 #Lienol packages
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #PassWall依赖
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #openwrt常用软件包不定期更新kenzo
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #Passwall
-sed -i '$a src-git openwrt_passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git openwrt_passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #liuruan001 pasckages
 #sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 #ssrplus
