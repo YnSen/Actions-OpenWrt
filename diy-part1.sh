1e63b6749244d091eb41d3376c7479f9e05ec498
YnSen <65432445+YnSen@users.noreply.github.com>
2021-07-09 11:09:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,3 +27,7 @@
 #sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 #ssrplus
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+#ubuntu20.04
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
