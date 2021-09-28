f3e88e154648a7c399ac7a8084814966b2aec8f2
YnSen <65432445+YnSen@users.noreply.github.com>
2021-09-28 13:46:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,5 @@
 git checkout v21.02.0
-#sed -i 's/src-git luci/#src-git luci/g' feeds.conf.default
-#Add immortal appsource
-#sed -i '$a src-git luci https://github.com/immortalwrt/luci.git^6f24422' feeds.conf.default
-#src-git passwall https://github.com/xiaorouji/openwrt-passwall.git
+sed -i 's/src-git luci/#src-git luci/g' feeds.conf.default
+sed -i 's/src-git packages/#src-git packages/g' feeds.conf.default
+sed -i '$a src-git luci https://github.com/immortalwrt/luci.git^31a0e8a' feeds.conf.default
+sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^7ce5631' feeds.conf.default
