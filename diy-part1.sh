b1e2294e4a5f60c63b46a532e34f79426e9ae2ee
YnSen <65432445+YnSen@users.noreply.github.com>
2021-09-29 12:30:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,5 @@
 git checkout v21.02.0
-sed -i 's/src-git luci/#src-git luci/g' feeds.conf.default
-sed -i 's/src-git packages/#src-git packages/g' feeds.conf.default
-sed -i '$a src-git luci https://github.com/immortalwrt/luci.git^31a0e8a' feeds.conf.default
-sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^7ce5631' feeds.conf.default
+#sed -i 's/src-git luci/#src-git luci/g' feeds.conf.default
+#sed -i 's/src-git packages/#src-git packages/g' feeds.conf.default
+#sed -i '$a src-git luci https://github.com/immortalwrt/luci.git^31a0e8a' feeds.conf.default
+#sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^7ce5631' feeds.conf.default
