498ec8ade918a0518f17d0ff2b68e584e4397f76
YnSen <65432445+YnSen@users.noreply.github.com>
2021-12-10 19:33:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,7 @@
 git checkout v21.02.1
 svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
 svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
-#x86immortalwrt uboot & target upstream
-rm -rf ./target/linux/x86
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/target/linux/x86 target/linux/86
+
 
 # Rockchip - immortalwrt uboot & target upstream
 rm -rf ./target/linux/rockchip
