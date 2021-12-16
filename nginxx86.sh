74d4efc015833c851a69d7949f0c9b79db58d1de
YnSen <65432445+YnSen@users.noreply.github.com>
2021-12-17 06:53:37 +0800

    Update nginxx86.sh

--- nginxx86.sh
+++ nginxx86.sh
@@ -42,7 +42,43 @@ svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
 #svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/target/linux/rockchip target/linux/rockchip
 #svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/boot/uboot-rockchip package/boot/uboot-rockchip
 #svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/boot/arm-trusted-firmware-rockchip-vendor package/boot/arm-trusted-firmware-rockchip-vendor
-#rm -f package/kernel/linux/modules/video.mk
+#rm -f package/kernel/linux/modules/video.mk##offical nginx x86
+
+# get source
+
+git clone https://github.com/openwrt/openwrt -b openwrt-21.02
+
+cd ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt
+
+git checkout v21.02.1
+
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+cp ~/work/Actions-OpenWrt/Actions-OpenWrt/patch/652-netfilter-flow_offload-add-check-ifindex.patch ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/target/linux/generic/hack-5.4/652-netfilter-flow_offload-add-check-ifindex.patch
+
+pushd target/linux/generic/hack-5.4
+
+chmod +x 652-netfilter-flow_offload-add-check-ifindex.patch
+
+git appply 652-netfilter-flow_offload-add-check-ifindex.patch
+
+popd
+
+# Drop uhttpd
+
+#pushd feeds/luci
+
+#curl -s https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/patch/0002-feeds-luci-Drop-uhttpd-depends.patch > 0002-feeds-luci-Drop-uhttpd-depends.patch
+
+#git apply 0002-feeds-luci-Drop-uhttpd-depends.patch && rm 0002-feeds-luci-Drop-uhttpd-depends.patch
+
+#popd
+
+# Update nginx-1.20.2
+
+#pushd feeds/packages
+
+#curl -s https://raw.githubuserconten
 #curl -sL https://github.com/immortalwrt/immortalwrt/raw/openwrt-21.02/package/kernel/linux/modules/video.mk > package/kernel/linux/modules/video.mk
 
 #curl -fL -o sdk.tar.xz https://downloads.openwrt.org/releases/21.02.1/targets/x86/64/openwrt-sdk-21.02.1-x86-64_gcc-8.4.0_musl.Linux-x86_64.tar.xz || wget -cO sdk.tar.xz https://downloads.openwrt.org/releases/21.02.1/targets/x86/64/openwrt-sdk-21.02.1-x86-64_gcc-8.4.0_musl.Linux-x86_64.tar.xz
