358f7fe72532ef90a25cc4aece651f6303c453c5
YnSen <65432445+YnSen@users.noreply.github.com>
2021-12-17 14:25:59 +0800

    Add files via upload

---
+++ sh/scripts/99_clean_build_cache.sh
@@ -0,0 +1,6 @@
+rm -rf bin/packages/aarch64_generic
+rm -rf bin/targets/rockchip/armv8
+rm -rf build_dir/target-aarch64_generic_musl/root-rockchip
+rm -rf build_dir/target-aarch64_generic_musl/root.orig-rockchip
+rm -rf staging_dir/target-aarch64_generic_musl/root-rockchip
+rm -rf staging_dir/packages/rockchip
