3a50fd6ccf6851dfa490e71eb712f4f795e5bce4
YnSen <65432445+YnSen@users.noreply.github.com>
2021-12-17 19:44:43 +0800

    Update uhttpx86.sh

--- sh/uhttpx86.sh
+++ sh/uhttpx86.sh
@@ -202,9 +202,9 @@ sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/p
 #rm -rf feeds/luci/applications/luci-app-mjpg-streamer
 #git clone https://github.com/sbwml/luci-app-mjpg-streamer feeds/luci/applications/luci-app-mjpg-streamer
 
-#curl -O https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/sh/02-remove_upx.sh
-#chmod 0755 *sh
-#./02-remove_upx.sh
+curl -O https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/sh/scripts/02-remove_upx.sh
+chmod 0755 *sh
+./02-remove_upx.sh
 
 cd ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt
 curl -s https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/conf/uhttpd.config > .config
