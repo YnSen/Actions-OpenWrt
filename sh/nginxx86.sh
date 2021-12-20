c2835862db32fe8b6eeeba7dff7ec6cb4618b0de
YnSen <65432445+YnSen@users.noreply.github.com>
2021-12-20 23:33:09 +0800

    Update nginxx86.sh

--- sh/nginxx86.sh
+++ sh/nginxx86.sh
@@ -239,8 +239,10 @@ sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/p
 #git clone https://github.com/sbwml/luci-app-mjpg-streamer feeds/luci/applications/luci-app-mjpg-streamer
 
 curl -O https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/sh/scripts/02-remove_upx.sh
+curl -O https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/sh/scripts/04-create_acl_for_luci.sh
 chmod 0755 *sh
 ./02-remove_upx.sh
+./04-create_acl_for_luci.sh
 
 curl -s https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/conf/nginx.config > .config
 make defconfig
