062cd3678eb47a87931ce1710da69b86eb748ef6
YnSen <65432445+YnSen@users.noreply.github.com>
2021-12-23 10:43:49 +0800

    Update ledex86.sh

--- sh/ledex86.sh
+++ sh/ledex86.sh
@@ -94,5 +94,7 @@ sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 #删除默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
-curl -s https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/conf/ledex86.config > .config
+cd ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt
+cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/ledex86.config ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/
+mv ledex86.config .config
 make defconfig
