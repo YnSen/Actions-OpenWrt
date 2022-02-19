46c94bd47b2983d708a3bd0810a22dc453877187
YnSen <65432445+YnSen@users.noreply.github.com>
2022-02-19 09:53:44 +0800

    Update uhttpx86.sh

--- sh/uhttpx86.sh
+++ sh/uhttpx86.sh
@@ -299,5 +299,6 @@ chmod 0755 *sh
 
 cd ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt
 cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/uhttpx86.config ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/
+mv ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/config-5.4 ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/target/linux/x86/
 mv uhttpx86.config .config
 make defconfig
