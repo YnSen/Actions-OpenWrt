24b60cd6140d79a92c74bd56431ab7de57a80291
YnSen <65432445+YnSen@users.noreply.github.com>
2022-03-25 17:15:31 +0800

    Update immr3p.sh

--- sh/immr3p.sh
+++ sh/immr3p.sh
@@ -3,6 +3,13 @@ git clone https://github.com/immortalwrt/immortalwrt.git openwrt
 cd ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
+# AdGuardHome
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+rm -rf feeds/packages/net/adguardhome
+svn co https://github.com/openwrt/packages/trunk/net/adguardhome feeds/packages/net/adguardhome
+sed -i '/\t)/a\\t$(STAGING_DIR_HOST)/bin/upx --lzma --best $(GO_PKG_BUILD_BIN_DIR)/AdGuardHome' ./feeds/packages/net/adguardhome/Makefile
+sed -i '/init/d' feeds/packages/net/adguardhome/Makefile
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
