cf2b09c096ac0a6512d1cbc61bf8fc731275456b
YnSen <65432445+YnSen@users.noreply.github.com>
2022-04-03 20:18:27 +0800

    Update uhttpr3p.sh

--- sh/uhttpr3p.sh
+++ sh/uhttpr3p.sh
@@ -29,12 +29,10 @@ sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 cp ~/work/Actions-OpenWrt/Actions-OpenWrt/patch/652-netfilter-flow_offload-add-check-ifindex.patch target/linux/generic/hack-5.4/
 
-#替换package/kernel/linux/modules/netsupport.mk添加kmod-inet-diag支持
-rm -rf package/kernel/linux/modules/netsupport.mk
-pushd package/kernel/linux/modules/
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel/linux/modules/netsupport.mk
-popd
-
+#添加kmod-inet-diag支持
+cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/inet-diag ./
+sed -i '/\$(call KernelPackage,netlink-diag))/r inet-diag' package/kernel/linux/modules/netsupport.mk
+rm -rf inet-diag
 
 # 默认设置
 #svn co https://github.com/YnSen/Actions-OpenWrt/trunk/default-settings package/default-settings
