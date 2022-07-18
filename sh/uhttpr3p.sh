a775fed4f7599b6b52a38d2101fc091f3fdd0fed
YnSen <65432445+YnSen@users.noreply.github.com>
2022-07-18 08:08:29 +0800

    Update uhttpr3p.sh

--- sh/uhttpr3p.sh
+++ sh/uhttpr3p.sh
@@ -7,15 +7,6 @@ git checkout v21.02.3
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-# Rockchip - immortalwrt uboot & target upstream
-#rm -rf ./target/linux/rockchip
-#rm -rf ./package/boot/uboot-rockchip
-#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/target/linux/rockchip target/linux/rockchip
-#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/boot/uboot-rockchip package/boot/uboot-rockchip
-#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/boot/arm-trusted-firmware-rockchip-vendor package/boot/arm-trusted-firmware-rockchip-vendor
-#rm -f package/kernel/linux/modules/video.mk
-#curl -sL https://github.com/immortalwrt/immortalwrt/raw/openwrt-21.02/package/kernel/linux/modules/video.mk > package/kernel/linux/modules/video.mk
-
 #ramips - immortalwrt uboot & target upstream
 #rm -rf ./target/linux/ramips
 #rm -rf ./package/boot/uboot-ramips
@@ -27,17 +18,15 @@ git checkout v21.02.3
 # Max connection limite
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
-cp ~/work/Actions-OpenWrt/Actions-OpenWrt/patch/652-netfilter-flow_offload-add-check-ifindex.patch target/linux/generic/hack-5.4/
-
 # 默认设置
-svn co https://github.com/YnSen/Actions-OpenWrt/trunk/default-settings package/default-settings
-pushd package/default-settings
-cp -r files ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/
-cd default/
-rm -rf zzz-default-settings
-rm -rf zzz-default-settingsnginx
-mv r3p zzz-default-settings
-popd
+#svn co https://github.com/YnSen/Actions-OpenWrt/trunk/default-settings package/default-settings
+#pushd package/default-settings
+#cp -r files ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/
+#cd default/
+#rm -rf zzz-default-settings
+#rm -rf zzz-default-settingsnginx
+#mv r3p zzz-default-settings
+#popd
 
 # 更换 golang 版本
 rm -rf ./feeds/packages/lang/golang
@@ -73,11 +62,6 @@ popd
 
 #openclash插件
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-
-#强制关机插件
-git clone https://github.com/esirplayground/luci-app-poweroff
-#自动关机插件
-  git clone https://github.com/sirpdboy/luci-app-autopoweroff
 
 # argon主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
@@ -116,43 +100,11 @@ ln -sv ../../../feeds/packages/net/softethervpn5 ./
 popd
 
 #kodexplorer
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-kodexplorer feeds/luci/applications/luci-app-kodexplorer
-pushd package/feeds/luci
-ln -sv ../../../feeds/luci/applications/luci-app-kodexplorer ./
-popd
+#svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-kodexplorer feeds/luci/applications/luci-app-kodexplorer
+#pushd package/feeds/luci
+#ln -sv ../../../feeds/luci/applications/luci-app-kodexplorer ./
+#popd
 
-# qBittorrent
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-qbittorrent feeds/luci/applications/luci-app-qbittorrent
-pushd package/feeds/luci
-ln -sv ../../../feeds/luci/applications/luci-app-qbittorrent ./
-popd
-
-svn co https://github.com/coolsnowwolf/packages/trunk/net/qBittorrent-static feeds/packages/net/qBittorrent-static
-pushd package/feeds/packages
-ln -sv ../../../feeds/packages/net/qBittorrent-static ./
-popd
-
-svn co https://github.com/coolsnowwolf/packages/trunk/net/qBittorrent feeds/packages/net/qBittorrent
-pushd package/feeds/packages
-ln -sv ../../../feeds/packages/net/qBittorrent ./
-popd
-
-
-svn co https://github.com/coolsnowwolf/packages/trunk/libs/qtbase feeds/packages/libs/qtbase
-pushd package/feeds/packages
-ln -sv ../../../feeds/packages/libs/qtbase ./
-popd
-
-
-svn co https://github.com/coolsnowwolf/packages/trunk/libs/qttools feeds/packages/libs/qttools
-pushd package/feeds/packages
-ln -sv ../../../feeds/packages/libs/qttools ./
-popd
-
-svn co https://github.com/coolsnowwolf/packages/trunk/libs/rblibtorrent feeds/packages/libs/rblibtorrent
-pushd package/feeds/packages
-ln -sv ../../../feeds/packages/libs/rblibtorrent ./
-popd
 
 # 应用过滤
 git clone https://github.com/sbwml/OpenAppFilter --depth=1 package/new/OpenAppFilter
@@ -171,10 +123,6 @@ git clone https://github.com/sbwml/OpenAppFilter --depth=1 package/new/OpenAppFi
 # wrt bw monitor
 git clone -b master --single-branch https://github.com/brvphoenix/wrtbwmon --depth=1 package/new/wrtbwmon
 git clone -b master --single-branch https://github.com/brvphoenix/luci-app-wrtbwmon --depth=1 package/new/luci-app-wrtbwmon
-
-# iputils
-#svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/utils/iputils package/network/utils/iputils
-
 
 # 迅雷快鸟
 git clone --depth 1 https://github.com/garypang13/luci-app-xlnetacc.git package/lean/luci-app-xlnetacc
@@ -260,35 +208,6 @@ popd
 # SSR Plus - deps
 svn co https://github.com/coolsnowwolf/packages/trunk/net/redsocks2 package/lean/helloworld/redsocks2
 
-# PASSWALL
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/passwall/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocksr-libev package/passwall/shadowsocksr-libev
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/passwall/microsocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/passwall/dns2socks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/passwall/dns2tcp
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/passwall/ipt2socks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan package/passwall/trojan
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/passwall/tcping
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/passwall/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/passwall/chinadns-ng
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/passwall/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/passwall/microsocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/passwall/naiveproxy
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/passwall/pdnsd-alt
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/simple-obfs package/passwall/simple-obfs
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/passwall/ssocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/passwall/tcping
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/passwall/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-core package/passwall/v2ray-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/passwall/v2ray-geodata
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/passwall/v2ray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/passwall/xray-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-plugin package/passwall/xray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocks-rust package/passwall/shadowsocks-rust
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/passwall/hysteria
-sed -i 's,一般般,通用,g' package/passwall/luci-app-passwall/po/zh-cn/passwall.po
-
-
 #aliyundrive-webdav
 svn co https://github.com/coolsnowwolf/packages/trunk/multimedia/aliyundrive-webdav feeds/packages/multimedia/aliyundrive-webdav
 pushd package/feeds/packages
@@ -300,14 +219,6 @@ pushd package/feeds/luci
 ln -sv ../../../feeds/luci/applications/luci-app-aliyundrive-webdav ./
 popd
 
-#UnblockMusic163
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git feeds/luci/applications/luci-app-unblockneteasemusic
-pushd package/feeds/luci
-ln -sv ../../../feeds/luci/applications/luci-app-unblockneteasemusic ./
-popd
-
-#为网易云添加ucode支持
-svn export https://github.com/openwrt/openwrt/trunk/package/utils/ucode package/utils/ucode
 
 #OpenVpnServer
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-openvpn-server feeds/luci/applications/luci-app-openvpn-server
@@ -377,12 +288,14 @@ popd
 #luci socat
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat package/new/luci-app-socat
 
-
-
-# 翻译
-#sed -i 's,发送,Transmission,g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
-#sed -i 's,frp 服务器,FRP 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
-#sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
+# FRP 内网穿透
+rm -rf ./feeds/luci/applications/luci-app-frps
+rm -rf ./feeds/luci/applications/luci-app-frpc
+rm -rf ./feeds/packages/net/frp
+rm -f ./package/feeds/packages/frp
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-frps package/lean/luci-app-frps
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-frpc package/lean/luci-app-frpc
+svn export https://github.com/coolsnowwolf/packages/trunk/net/frp package/lean/frp
 
 # mjpg-streamer init
 #sed -i "s,option port '8080',option port '1024',g" feeds/packages/multimedia/mjpg-streamer/files/mjpg-streamer.config
@@ -392,51 +305,14 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat package/ne
 #rm -rf feeds/luci/applications/luci-app-mjpg-streamer
 #git clone https://github.com/sbwml/luci-app-mjpg-streamer feeds/luci/applications/luci-app-mjpg-streamer
 
-# 更换 Nodejs 版本
-rm -rf ./feeds/packages/lang/node
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node feeds/packages/lang/node
-sed -i '\/bin\/node/a\\t$(STAGING_DIR_HOST)/bin/upx --lzma --best $(1)/usr/bin/node' feeds/packages/lang/node/Makefile
-rm -rf ./feeds/packages/lang/node-arduino-firmata
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-arduino-firmata feeds/packages/lang/node-arduino-firmata
-rm -rf ./feeds/packages/lang/node-cylon
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-cylon feeds/packages/lang/node-cylon
-rm -rf ./feeds/packages/lang/node-hid
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-hid feeds/packages/lang/node-hid
-rm -rf ./feeds/packages/lang/node-homebridge
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-homebridge feeds/packages/lang/node-homebridge
-rm -rf ./feeds/packages/lang/node-serialport
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-serialport feeds/packages/lang/node-serialport
-rm -rf ./feeds/packages/lang/node-serialport-bindings
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-serialport-bindings feeds/packages/lang/node-serialport-bindings
-rm -rf feeds/packages/lang/node-yarn
-rm -rf package/feeds/packages/node-yarn
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-yarn feeds/packages/lang/node-yarn
-pushd package/feeds/packages/
-ln -sv ../../../feeds/packages/lang/node-yarn ./
-popd
 
-rm -rf feeds/packages/lang/node-serialport-bindings-cpp
-rm -rf package/feeds/packages/node-serialport-bindings-cpp
-svn export https://github.com/nxhack/openwrt-node-packages/trunk/node-serialport-bindings-cpp feeds/packages/lang/node-serialport-bindings-cpp
-pushd package/feeds/packages/
-ln -sv ../../../feeds/packages/lang/node-serialport-bindings-cpp ./
-popd
-
-curl -O https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/sh/scripts/02-remove_upx.sh
-curl -O https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/sh/scripts/03-convert_translation.sh
-curl -O https://raw.githubusercontent.com/YnSen/Actions-OpenWrt/main/sh/scripts/04-create_acl_for_luci.sh
-chmod 0755 *sh
-./02-remove_upx.sh
-./03-convert_translation.sh
-./04-create_acl_for_luci.sh -a
-
-cd ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt
-cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/uhttpr3p.config ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/
-mv uhttpr3p.config .config
+#cd ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt
+#cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/uhttpr3p.config ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/
+#mv uhttpr3p.config .config
 
 #添加kmod-inet-diag支持
 cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/inet-diag ./
 sed -i '/\$(call KernelPackage,netlink-diag))/r inet-diag' package/kernel/linux/modules/netsupport.mk
 rm -rf inet-diag
 
-make defconfig
+#make defconfig
