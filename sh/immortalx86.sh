c4cfe5a9702f09bef85db4af97155902d04dd02a
YnSen <65432445+YnSen@users.noreply.github.com>
2022-09-20 11:19:15 +0800

    Update immortalx86.sh

--- sh/immortalx86.sh
+++ sh/immortalx86.sh
@@ -95,6 +95,7 @@ cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/imm/immx86.config ~/work/Actions-
 cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/imm/config-5.4 ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/target/linux/x86/
 cp ~/work/Actions-OpenWrt/Actions-OpenWrt/conf/imm/99-default-settings ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/emortal/default-settings/files/
 cp ~/work/Actions-OpenWrt/Actions-OpenWrt/sh/scripts/03-convert_translation.sh ~/Actions-OpenWrt/openwrt/
+chmod 0755 *sh
 ./03-convert_translation.sh
 mv nginx.config .config
 make defconfig
